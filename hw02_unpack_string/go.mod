58308c11a4b51f2253c14118b45dd76029e90d6c
Denis Kuzin <den.kuzin33@yandex.ru>
2023-04-19 11:04:20 +0300

    HW2 is completed

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/DenKuzin33/OtusHw/hw02_unpack_string
 
 go 1.19
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(source string) (string, error) {
+	var sb strings.Builder
+	var currentChar string
+
+	runes := []rune(source)
+
+	if len(runes) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+
+	for _, v := range runes {
+		if unicode.IsDigit(v) {
+			if currentChar == "" {
+				return "", ErrInvalidString
+			}
+
+			charCount, _ := strconv.Atoi(string(v))
+			sb.WriteString(strings.Repeat(currentChar, charCount))
+			currentChar = ""
+		} else {
+			if currentChar != "" {
+				sb.WriteString(currentChar)
+				currentChar = ""
+			}
+			currentChar = string(v)
+		}
+	}
+
+	if !unicode.IsDigit(runes[len(runes)-1]) {
+		sb.WriteString(string(runes[len(runes)-1]))
+	}
+
+	return sb.String(), nil
 }
