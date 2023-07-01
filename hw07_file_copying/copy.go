5bacaf7a97f6435c78d3ffefdb9c2a7623ff9306
Denis Kuzin <den.kuzin33@yandex.ru>
2023-07-01 22:16:09 +0300

    HW07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
 )
 
 var (
@@ -9,7 +13,77 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+type progressBar struct {
+	TotalBytesCount   int
+	CurrentBytesCount int
+}
+
+func (pb *progressBar) Write(p []byte) (n int, err error) {
+	pLen := len(p)
+	pb.CurrentBytesCount += pLen
+	fmt.Printf("%d%%\n", (pb.CurrentBytesCount*100)/pb.TotalBytesCount)
+	return pLen, nil
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	source, err := os.OpenFile(fromPath, os.O_RDONLY, 0o444)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer source.Close()
+
+	sourceInfo, err := source.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	sourceLen := sourceInfo.Size()
+	bytesToCopy := sourceLen - offset
+	if limit != 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	if offset > sourceLen {
+		return ErrOffsetExceedsFileSize
+	}
+
+	target, err := os.Create(toPath)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer target.Close()
+
+	// Подбираем подходящий под количество байт размер буфера
+	var bufferSize int
+	if bytesToCopy < 100 {
+		bufferSize = 4
+	} else {
+		bufferSize = int(math.Pow(2, math.Ceil(math.Log(float64(bytesToCopy/100))/math.Log(2))))
+	}
+	buffer := make([]byte, bufferSize)
+	pb := progressBar{TotalBytesCount: int(bytesToCopy)}
+	multiWriter := io.MultiWriter(target, &pb)
+
+	if offset > 0 {
+		_, err := source.Seek(offset, 0)
+		if err != nil {
+			return err
+		}
+	}
+	for pb.CurrentBytesCount < int(bytesToCopy) {
+		_, err := source.Read(buffer)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		remained := bytesToCopy - int64(pb.CurrentBytesCount)
+		if remained < int64(bufferSize) {
+			multiWriter.Write(buffer[:remained])
+		} else {
+			multiWriter.Write(buffer)
+		}
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,72 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	outFilePath := "testdata/outFile.txt"
+
+	expected1, _ := os.OpenFile("testdata/out_offset0_limit0.txt", os.O_RDONLY, 0o444)
+	expected2, _ := os.OpenFile("testdata/out_offset0_limit10.txt", os.O_RDONLY, 0o444)
+	expected3, _ := os.OpenFile("testdata/out_offset0_limit1000.txt", os.O_RDONLY, 0o444)
+	expected4, _ := os.OpenFile("testdata/out_offset0_limit10000.txt", os.O_RDONLY, 0o444)
+	expected5, _ := os.OpenFile("testdata/out_offset100_limit1000.txt", os.O_RDONLY, 0o444)
+	expected6, _ := os.OpenFile("testdata/out_offset6000_limit1000.txt", os.O_RDONLY, 0o444)
+
+	testData := []struct {
+		offset, limit int
+		expected      *os.File
+	}{
+		{0, 0, expected1},
+		{0, 10, expected2},
+		{0, 1000, expected3},
+		{0, 10000, expected4},
+		{100, 1000, expected5},
+		{6000, 1000, expected6},
+	}
+	defer os.Remove(outFilePath)
+
+	for _, data := range testData {
+		t.Run(data.expected.Name(), func(t *testing.T) {
+			Copy("testdata/input.txt", outFilePath, int64(data.offset), int64(data.limit))
+			require.True(t, deepCompare(data.expected.Name(), outFilePath))
+		})
+	}
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outFilePath, 99999, 0)
+		require.Error(t, err)
+	})
+}
+
+func deepCompare(file1, file2 string) bool {
+	source, err := os.Open(file1)
+	if err != nil {
+		return false
+	}
+	defer source.Close()
+
+	target, err := os.Open(file2)
+	if err != nil {
+		return false
+	}
+	defer target.Close()
+
+	sscan := bufio.NewScanner(source)
+	tscan := bufio.NewScanner(target)
+
+	for sscan.Scan() {
+		tscan.Scan()
+		if !bytes.Equal(sscan.Bytes(), tscan.Bytes()) {
+			return false
+		}
+	}
+
+	return true
 }
