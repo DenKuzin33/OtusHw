17267ff81ed234c4549bf1f7eba5b98975341039
Denis Kuzin <den.kuzin33@yandex.ru>
2023-08-22 22:44:45 +0300

    HW09

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,190 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
 
+func (v ValidationError) String() string {
+	return fmt.Sprintf("%s:%s", v.Field, v.Err)
+}
+
 type ValidationErrors []ValidationError
 
+var (
+	errNotStruct      = errors.New("v is not a struct")
+	errLessThanMin    = errors.New("value is less than min")
+	errGreaterThanMax = errors.New("value is greater than max")
+	errNotInSet       = errors.New("value is not in set")
+	errLenNotMatch    = errors.New("string length doesn't match")
+	errRegexNotMatch  = errors.New("string doesn't match a regex")
+)
+
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	sb := strings.Builder{}
+	for i, err := range v {
+		if i > 0 {
+			sb.WriteString("\n")
+		}
+		sb.WriteString(err.String())
+	}
+	return sb.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	vType := reflect.TypeOf(v)
+	vValue := reflect.ValueOf(v)
+
+	if vType.Kind() != reflect.Struct {
+		return errNotStruct
+	}
+
+	validationErrors := ValidationErrors{}
+
+	for i := 0; i < vType.NumField(); i++ {
+		fieldType := vType.Field(i)
+		fieldVal := vValue.Field(i)
+		constraints := strings.Split(fieldType.Tag.Get("validate"), "|")
+
+		switch fieldType.Type.Kind() { //nolint:exhaustive
+		case reflect.Int:
+			validationResult, err := validateInt(fieldVal.Interface().(int), fieldType.Name, constraints)
+			if err != nil {
+				return err
+			}
+			validationErrors = append(validationErrors, validationResult...)
+		case reflect.String:
+			validationResult, err := validateString(fieldVal.String(), fieldType.Name, constraints)
+			if err != nil {
+				return err
+			}
+			validationErrors = append(validationErrors, validationResult...)
+		case reflect.Array:
+			validationResult, err := validateArray(fieldType, fieldVal, constraints)
+			if err != nil {
+				return err
+			}
+			validationErrors = append(validationErrors, validationResult...)
+		}
+	}
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
 	return nil
+}
+
+func validateInt(number int, name string, rules []string) (ValidationErrors, error) {
+	var result ValidationErrors
+	for _, rule := range rules {
+		keyValue := strings.Split(rule, ":")
+
+		switch keyValue[0] {
+		case "min":
+			ruleValue, err := strconv.Atoi(keyValue[1])
+			if err != nil {
+				return nil, err
+			}
+			if number < ruleValue {
+				result = append(result, ValidationError{Field: name, Err: errLessThanMin})
+			}
+		case "max":
+			ruleValue, err := strconv.Atoi(keyValue[1])
+			if err != nil {
+				return nil, err
+			}
+			if number > ruleValue {
+				result = append(result, ValidationError{Field: name, Err: errGreaterThanMax})
+			}
+		case "in":
+			set := strings.Split(keyValue[1], ",")
+			numInSet := false
+
+			for _, strVal := range set {
+				val, err := strconv.Atoi(strVal)
+				if err != nil {
+					return nil, err
+				}
+				if number == val {
+					numInSet = true
+					break
+				}
+			}
+			if !numInSet {
+				result = append(result, ValidationError{Field: name, Err: errNotInSet})
+			}
+		}
+	}
+
+	return result, nil
+}
+
+func validateString(str string, name string, rules []string) (ValidationErrors, error) {
+	var result ValidationErrors
+	for _, rule := range rules {
+		keyValue := strings.Split(rule, ":")
+
+		switch keyValue[0] {
+		case "len":
+			strLen, err := strconv.Atoi(keyValue[1])
+			if err != nil {
+				return nil, err
+			}
+			if len(str) != strLen {
+				result = append(result, ValidationError{Field: name, Err: errLenNotMatch})
+			}
+		case "regexp":
+			regex, err := regexp.Compile(keyValue[1])
+			if err != nil {
+				return nil, err
+			}
+			if !regex.MatchString(str) {
+				result = append(result, ValidationError{Field: name, Err: errRegexNotMatch})
+			}
+		case "in":
+			set := strings.Split(keyValue[1], ",")
+			strInSet := false
+			for _, val := range set {
+				if str == val {
+					strInSet = true
+					break
+				}
+			}
+			if !strInSet {
+				result = append(result, ValidationError{Field: name, Err: errNotInSet})
+			}
+		}
+	}
+	return result, nil
+}
+
+func validateArray(fieldType reflect.StructField, fieldVal reflect.Value, constraints []string) (ValidationErrors, error) { //nolint:lll
+	validationErrors := ValidationErrors{}
+	switch fieldType.Type.Elem().Kind() { //nolint:exhaustive
+	case reflect.Int:
+		for _, val := range fieldVal.Interface().([]int) {
+			validationResult, err := validateInt(val, fieldType.Name, constraints)
+			if err != nil {
+				return nil, err
+			}
+			validationErrors = append(validationErrors, validationResult...)
+		}
+	case reflect.String:
+		for _, str := range fieldVal.Interface().([]string) {
+			validationResult, err := validateString(str, fieldType.Name, constraints)
+			if err != nil {
+				return nil, err
+			}
+			validationErrors = append(validationErrors, validationResult...)
+		}
+	}
+	return validationErrors, nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -11,13 +13,14 @@ type UserRole string
 // Test the function on different structures and other types.
 type (
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
-		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		ID         string `json:"id" validate:"len:36"`
+		Name       string
+		Age        int             `validate:"min:18|max:50"`
+		Reputation int             `validate:"min:0|max:99"`
+		Email      string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role       UserRole        `validate:"in:admin,stuff"`
+		Phones     []string        `validate:"len:11"`
+		meta       json.RawMessage //nolint:unused
 	}
 
 	App struct {
@@ -42,10 +45,61 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			User{
+				ID:         "1234567890",
+				Age:        51,
+				Reputation: -1,
+				Email:      "biba.boba@yandex.ru",
+				Role:       "stuf",
+				Phones:     []string{"8920142288"},
+			},
+			fmt.Errorf("ID:%w\nAge:%w\nReputation:%w\nEmail:%w\nRole:%w", errLenNotMatch, errGreaterThanMax, errLessThanMin, errRegexNotMatch, errNotInSet), //nolint:lll
 		},
-		// ...
-		// Place your code here.
+		{
+			User{
+				ID:         "123456789012345678901234567890123456",
+				Age:        26,
+				Reputation: 30,
+				Email:      "bibaboba@yandex.ru",
+				Role:       "stuff",
+				Phones:     []string{"89201422888"},
+			},
+			nil,
+		},
+		{
+			App{
+				Version: "123456",
+			},
+			fmt.Errorf("Version:%w", errLenNotMatch),
+		},
+		{
+			App{
+				Version: "12345",
+			},
+			nil,
+		},
+		{
+			Token{
+				Header:    []byte("header"),
+				Payload:   []byte("payload"),
+				Signature: []byte("signature"),
+			},
+			nil,
+		},
+		{
+			Response{
+				Code: 228,
+				Body: "qwertyuiop[]",
+			},
+			fmt.Errorf("Code:%w", errNotInSet),
+		},
+		{
+			Response{
+				Code: 404,
+				Body: "qwertyuiop[]",
+			},
+			nil,
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +107,12 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			if err != nil {
+				require.Equal(t, tt.expectedErr.Error(), err.Error())
+			} else {
+				require.Nil(t, tt.expectedErr)
+			}
 		})
 	}
 }
